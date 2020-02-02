03a81d7beb9cf57fa670c19ded96910b59615b77
desperatecat <376198935@qq.com>
2020-02-01 20:41:07 -0500

    add post reducer

--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -25,6 +25,12 @@ export default function(state = initialState, action) {
         posts: action.payload,
         loading: false
       };
+
+    case ADD_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts] //add a new post
+      };
     default:
       return state;
   }
